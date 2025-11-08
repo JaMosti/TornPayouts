ec32afd12efd9b6ed83c70fa712c069b3a64d369
Kamil M <kamostowski@gmail.com>
2025-11-08 09:41:41 +0100

    adjust pay in and after war

--- get_payout.py
+++ get_payout.py
@@ -7,18 +7,18 @@ from pathlib import Path
 
 
 # --- Edit
-enemy_faction = 8766
-war_from = "15:00:17 25/10/25"
-war_to = "19:22:05 30/10/25"
+enemy_faction = 13421
+war_from = "16:00:09 07/11/25"
+war_to = "08:20:16 08/11/25"
 key = ""        # https://www.torn.com/preferences.php#tab=api
-cf_factor = 1           # Price for crossfire hit
+cf_factor_during_war = 1            # Price for crossfire hit
+cf_factor_after_war = 1             # Price for crossfire hit
 payout_factor = 0.7     # How much goes to members
 
 # --- Configuration ---
 url = "https://api.torn.com/v2/faction/members"
 faction_id = 9176   # The Iron Fist ID
 crossfire = 37530   # Crossfire ID
-
 chain_milestones = [25, 50, 100, 250, 500, 1000, 2500, 5000, 10000, 25000, 50000, 100000]
 chain_points =     [20, 40, 80, 160, 320, 640, 1280, 2560, 5120, 10240, 20480, 40960]
 all_respect = 0
@@ -49,6 +49,8 @@ else:
 
 enemy_name = ""
 did_we_won = this_war["winner"] == faction_id
+war_end = this_war["end"]
+war_from = this_war["start"]
 url = f"https://api.torn.com/v2/faction/{this_war["id"]}/rankedwarreport"
 response = requests.get(url, params=params)
 data = response.json()
@@ -69,6 +71,7 @@ for faction in report["factions"]:
                 "respect_war": 0,
                 "attacks_crossfire": 0,
                 "respect_crossfire": 0,
+                "respect_crossfire_adj": 0,
                 "adjusted_respect": 0,
                 "payout": 0,
                 "payout_str": "",
@@ -84,6 +87,9 @@ for reward in rewards:
     data = response.json()
     item = data["itemmarket"]
     total_payout += item["item"]["average_price"] * reward[1]
+    pay_str = str(item["item"]["average_price"])
+    pay_new = [t if i % 3 or not i else f"{t}," for i, t in enumerate(pay_str[::-1])]
+    reward.append("".join(reversed(pay_new)))
 
 payout = total_payout * payout_factor
 pay_str = str(int(payout))
@@ -119,10 +125,15 @@ try:
                 if respect_gain:
                     if attack["defender"]["faction_id"] == enemy_faction:
                         output_data[attack["attacker"]["id"]]["attacks_war"] += 1
-                        output_data[attack["attacker"]["id"]]["respect_war"] += respect_gain
+                        adj = respect_gain if int(war_end) > int(attack["started"]) else 0
+                        output_data[attack["attacker"]["id"]]["respect_war"] += adj
                     elif attack["defender"]["faction_id"] == crossfire:
                         output_data[attack["attacker"]["id"]]["attacks_crossfire"] += 1
                         output_data[attack["attacker"]["id"]]["respect_crossfire"] += respect_gain
+                        adj = respect_gain * cf_factor_during_war \
+                                if int(war_end) > int(attack["started"]) \
+                                else respect_gain * cf_factor_after_war
+                        output_data[attack["attacker"]["id"]]["respect_crossfire_adj"] += respect_gain
 
         next_link = data["_metadata"]["links"]["next"]
         if len(attacks) < 1000:
@@ -133,7 +144,7 @@ except Exception as e:
 
 # === Get payout
 for member in output_data:
-    output_data[member]["adjusted_respect"] = output_data[member]["respect_war"] + output_data[member]["respect_crossfire"] * cf_factor
+    output_data[member]["adjusted_respect"] = output_data[member]["respect_war"] + output_data[member]["respect_crossfire_adj"]
     all_respect += output_data[member]["adjusted_respect"]
 for member in output_data:
     output_data[member]["payout"] = int(payout/all_respect*output_data[member]["adjusted_respect"])
@@ -166,5 +177,6 @@ html_str = template.render(
  
 out_path = Path("table.html").resolve()
 out_path.write_text(html_str, encoding="utf-8")
-
+enemy_name = enemy_name.replace(" ", "_")
 subprocess.run(f"weasyprint.exe table.html payouts_{enemy_name}.pdf")
+# os.remove("table.html")

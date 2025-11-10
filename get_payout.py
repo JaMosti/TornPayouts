1ae9bb9d13deedb4c2361af4f7d1d53a47d68b1c
Kamil M <kamostowski@gmail.com>
2025-11-10 22:44:03 +0100

    first version of HOF

--- get_payout.py
+++ get_payout.py
@@ -1,6 +1,7 @@
 import requests
 import subprocess
 from datetime import datetime, timezone
+import time
 import os
 from jinja2 import Environment, FileSystemLoader, select_autoescape
 from pathlib import Path
@@ -11,14 +12,13 @@ enemy_faction = 13421               # id of enemy faction - Copy from url
 war_from = "16:00:09 07/11/25"      # Chain died - Copy from faction log
 war_to = "09:00:25 08/11/25"        # War start - Copy from faction log
 key = "etrgwAdkvDZgHC8W"            # https://www.torn.com/preferences.php#tab=api
-cf_factor_during_war = 1            # Price for crossfire hit
-cf_factor_after_war = 1             # Price for crossfire hit
+raid_factor_during_war = 1            # Price for raid hit
+raid_factor_after_war = 1             # Price for raid hit
 payout_factor = 0.7                 # How much goes to members
 
 # --- Configuration ---
 url = "https://api.torn.com/v2/faction/members"
 faction_id = 9176   # The Iron Fist ID
-crossfire = 37530   # Crossfire ID
 chain_milestones = [25, 50, 100, 250, 500, 1000, 2500, 5000, 10000, 25000, 50000, 100000]
 chain_points =     [20, 40, 80, 160, 320, 640, 1280, 2560, 5120, 10240, 20480, 40960]
 all_respect = 0
@@ -69,10 +69,13 @@ for faction in report["factions"]:
                 "name": member["name"],
                 "attacks_war": 0,
                 "respect_war": 0,
-                "attacks_crossfire": 0,
-                "respect_crossfire": 0,
-                "respect_crossfire_adj": 0,
+                "attacks_raid": 0,
+                "respect_raid": 0,
+                "respect_raid_adj": 0,
                 "adjusted_respect": 0,
+                "attacks_below_2_ff": 0,
+                "assists": 0,
+                "overseas": 0,
                 "payout": 0,
                 "payout_str": "",
             }
@@ -96,55 +99,53 @@ pay_str = str(int(payout))
 pay_new = [t if i % 3 or not i else f"{t}," for i, t in enumerate(pay_str[::-1])]
 payout_str = "".join(reversed(pay_new))
 # === Get Hits
-url = "https://api.torn.com/v2/faction/attacksfull"
-next_link = f"{url}?filters=outgoing&limit=1000&sort=ASC&to={timestamp_to}&from={timestamp_from}"
+url = "https://api.torn.com/v2/faction/attacks"
+next_link = f"{url}?filters=outgoing&limit=100&sort=ASC&to={timestamp_to}&from={timestamp_from}"
 aaa = set()
-try:
-    while(next_link):
+while(next_link):
+    try:
         response = requests.get(next_link, params=params)
         data = response.json()
         if "attacks" in data:
             attacks = data["attacks"]
             for attack in attacks:
-                if attack['respect_gain'] in chain_points:
-                    a_url = "https://api.torn.com/v2/faction/attacks"
-                    a_link = f"{a_url}?filters=outgoing&limit=100&sort=ASC&from={attack['started']}"
-                    a_response = requests.get(a_link, params=params)
-                    a_data = a_response.json()
-                    if "attacks" in a_data:
-                        a_attacks = a_data["attacks"]
-                        for a_attack in a_attacks:
-                            if a_attack['code'] == attack['code']:
-                                if a_attack['chain'] in chain_milestones:
-                                    respect_gain = 10
-                                    break
-                        else:
-                            respect_gain = attack['respect_gain']
+
+                if attack['chain'] in chain_milestones:
+                    respect_gain = 10
                 else:
                     respect_gain = attack['respect_gain']
-                if respect_gain:
-                    if attack["defender"]["faction_id"] == enemy_faction:
-                        output_data[attack["attacker"]["id"]]["attacks_war"] += 1
-                        adj = respect_gain if int(war_end) > int(attack["started"]) else 0
-                        output_data[attack["attacker"]["id"]]["respect_war"] += adj
-                    elif attack["defender"]["faction_id"] == crossfire:
-                        output_data[attack["attacker"]["id"]]["attacks_crossfire"] += 1
-                        output_data[attack["attacker"]["id"]]["respect_crossfire"] += respect_gain
-                        adj = respect_gain * cf_factor_during_war \
-                                if int(war_end) > int(attack["started"]) \
-                                else respect_gain * cf_factor_after_war
-                        output_data[attack["attacker"]["id"]]["respect_crossfire_adj"] += respect_gain
+
+                if attack["is_ranked_war"] or attack["is_raid"]:
+                    if respect_gain:
+                        if attack["is_ranked_war"]:
+                            output_data[attack["attacker"]["id"]]["attacks_war"] += 1
+                            adj = respect_gain if int(war_end) > int(attack["started"]) else 0
+                            output_data[attack["attacker"]["id"]]["respect_war"] += adj
+                        elif attack["is_raid"]:
+                            output_data[attack["attacker"]["id"]]["attacks_raid"] += 1
+                            output_data[attack["attacker"]["id"]]["respect_raid"] += respect_gain
+                            adj = respect_gain * raid_factor_during_war \
+                                    if int(war_end) > int(attack["started"]) \
+                                    else respect_gain * raid_factor_after_war
+                            output_data[attack["attacker"]["id"]]["respect_raid_adj"] += respect_gain
+
+                        # Other stats
+                        output_data[attack["attacker"]["id"]]["overseas"] += attack["modifiers"]["overseas"] > 1
+                        output_data[attack["attacker"]["id"]]["attacks_below_2_ff"] += attack["modifiers"]["fair_fight"] < 2
+                    
+                    output_data[attack["attacker"]["id"]]["assists"] += attack["result"] == "Assist"
 
         next_link = data["_metadata"]["links"]["next"]
-        if len(attacks) < 1000:
+        if len(attacks) < 100:
             next_link = None
-except Exception as e:
-    print(e)
-    os.exit(0)
+    except Exception as e:
+        print(e)
+        print("Sleeping for 30 sec")
+        time.sleep(30)
 
 # === Get payout
 for member in output_data:
-    output_data[member]["adjusted_respect"] = output_data[member]["respect_war"] + output_data[member]["respect_crossfire_adj"]
+    output_data[member]["adjusted_respect"] = output_data[member]["respect_war"] + output_data[member]["respect_raid_adj"]
     all_respect += output_data[member]["adjusted_respect"]
 for member in output_data:
     output_data[member]["payout"] = int(payout/all_respect*output_data[member]["adjusted_respect"])
@@ -152,10 +153,20 @@ for member in output_data:
     pay_new = [t if i % 3 or not i else f"{t}," for i, t in enumerate(pay_str[::-1])]
     output_data[member]["payout_str"] = "".join(reversed(pay_new))
     output_data[member]["respect_war"] = int(output_data[member]["respect_war"])
-    output_data[member]["respect_crossfire"] = int(output_data[member]["respect_crossfire"])
+    output_data[member]["respect_raid"] = int(output_data[member]["respect_raid"])
     output_data[member]["adjusted_respect"] = int(output_data[member]["adjusted_respect"])
 
-# === Save to csv
+# === Get supports
+supports = [[output_data[member]["assists"], member, output_data[member]["name"]] for member in output_data if output_data[member]["assists"]]
+supports.sort(key=lambda x: x[0], reverse=True)
+# === Get baby bullies
+bullies = [[output_data[member]["attacks_below_2_ff"], output_data[member]["attacks_war"]+output_data[member]["attacks_raid"], member, output_data[member]["name"]] for member in output_data]
+bullies.sort(key=lambda x: x[0], reverse=True)
+# === Get globertrotters
+globertrotters = [[output_data[member]["overseas"], member, output_data[member]["name"]] for member in output_data if output_data[member]["overseas"]]
+globertrotters.sort(key=lambda x: x[0], reverse=True)
+
+# === Save to htmls
 columns = list(next(iter(output_data.values())).keys())
 table = sorted(list(output_data.values()), key=lambda x: x['payout'], reverse=True)
 tables = [table]
@@ -172,7 +183,10 @@ html_str = template.render(
     rewards  = rewards,
     payout  = payout_str,
     columns = columns,
-    tables  = tables
+    tables  = tables,
+    supports  = supports[:30],
+    bullies  = bullies[:30],
+    globertrotters  = globertrotters[:30],
 )
  
 out_path = Path("table.html").resolve()
